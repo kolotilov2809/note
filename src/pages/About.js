ca97fec50933986b38559404f15a4d5861fd2e6e
Alex <kolotilov2809@gmail.com>
2022-02-10 17:09:04 +0300

    project

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => (
+  <div className="jumbotron">
+    <div className="container">
+      <h1 className="display-4">Приложение для стажировки</h1>
+      <p className="lead">
+        У тебя все <strong>получится!</strong>
+      </p>
+    </div>
+  </div>
+)
